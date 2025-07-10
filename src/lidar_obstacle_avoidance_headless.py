528317a838118f5d072906e9b4083dcc9ef6d368
randomx10 <account1@mindustry.shop>
2025-07-10 02:00:54 -0500

    Update lidar_obstacle_avoidance_headless.py

--- src/lidar_obstacle_avoidance_headless.py
+++ src/lidar_obstacle_avoidance_headless.py
@@ -4,6 +4,21 @@ LiDAR Obstacle Avoidance System - Headless Version
 Designed to run automatically on system startup without user interaction.
 Integrates LiDAR HTTP server, motor control (eng.py), and servo rotation (rot.py)
 Automatically rotates when obstacle detected at 180 degrees with distance < 1500mm
+
+Copyright (C) 2025  Alexis Martinez
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 import time
